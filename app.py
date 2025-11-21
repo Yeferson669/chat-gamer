84f18c244f5fc4a73af3fb3e161688aacd9894db
Yeferson669 <Ydguaca49@ucatolica.edu.co>
2025-11-20 22:46:45 -0500

    chat gamer

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Estructura: { "fortnite": [ {user, text, time} ] }
+messages = {}
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.get("/api/messages")
+def get_messages():
+    room = request.args.get("room")
+    return jsonify(messages.get(room, []))
+
+
+@app.post("/api/messages")
+def send_message():
+    data = request.json
+    room = data["room"]
+
+    msg = {
+        "user": data["user"],
+        "text": data["text"],
+        "time": datetime.now().strftime("%H:%M")
+    }
+
+    messages.setdefault(room, []).append(msg)
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
